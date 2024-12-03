f746fe2ff176c1d91188164e94133977c3c728e3
SivaShankar1255 <155956695+AlabanuSivaShankar@users.noreply.github.com>
2024-12-03 21:03:16 +0530

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -4,6 +4,13 @@ import streamlit as st
 from PIL import Image
 
 
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.metrics import accuracy_score
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
 
 
 # Define patterns and responses
